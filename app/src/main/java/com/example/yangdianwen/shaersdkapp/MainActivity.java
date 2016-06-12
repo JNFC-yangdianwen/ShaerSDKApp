18ead2c18d97a1a134d86c5a2bc84f9c756fd7e9
JNFC-yangdianwen <2550286303@qq.com>
2016-06-12 14:28:28 +0800

    这是一个一键分享的demo可以分享到qq空间，微信朋友圈，微信好友，新浪微博

--- app/src/main/java/com/example/yangdianwen/shaersdkapp/MainActivity.java
+++ app/src/main/java/com/example/yangdianwen/shaersdkapp/MainActivity.java
@@ -4,39 +4,57 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
 import com.example.yangdianwen.shaersdkapp.onekeyshare.OnekeyShare;
+
 import cn.sharesdk.framework.ShareSDK;
-public class MainActivity extends AppCompatActivity {
+import cn.sharesdk.tencent.qq.QQ;
+import cn.sharesdk.tencent.weibo.TencentWeibo;
+import cn.sharesdk.wechat.favorite.WechatFavorite;
+import cn.sharesdk.wechat.friends.Wechat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mFrd;
     private Button mWeixin;
     private Button mQq;
     private Button mWeibo;
+    private OnekeyShare mOnekeyShare;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ShareSDK.initSDK(this);
+        mOnekeyShare = new OnekeyShare();
         mFrd = (Button) findViewById(R.id.button);
         mWeixin = (Button) findViewById(R.id.button2);
         mQq = (Button) findViewById(R.id.button3);
         mWeibo = (Button) findViewById(R.id.button4);
-        mFrd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                switch(v.getId()){
-                    case R.id.button:
-
-                        OnekeyShare onekeyShare=new OnekeyShare();
-
-                        onekeyShare.setTitle("一键分享");
-                                onekeyShare.setText("这是一键分享功能");
-//                        onekeyShare.setImageUrl("http//:www.baidu.com");
-                                        onekeyShare.show(MainActivity.this);
-                        break;
-                }
-            }
-        });
+        mFrd.setOnClickListener(this);
+        mQq.setOnClickListener(this);
+        mWeibo.setOnClickListener(this);
+        mWeixin.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                mOnekeyShare.setPlatform(WechatFavorite.NAME);
+                mOnekeyShare.show(MainActivity.this);
+                break;
+            case R.id.button2:
+                mOnekeyShare.setPlatform(Wechat.NAME);
+                mOnekeyShare.show(MainActivity.this);
+                break;
+            case R.id.button3:
+                mOnekeyShare.setPlatform(QQ.NAME);
+                mOnekeyShare.show(MainActivity.this);
+                break;
+            case R.id.button4:
+                mOnekeyShare.setPlatform(TencentWeibo.NAME);
+                mOnekeyShare.show(MainActivity.this);
+                break;
+        }
 
     }
 }
